4de78906fae2d05e28e6be4ceeb6320f24907567
BogdanGur <bogur007@gmail.com>
2022-04-19 21:49:10 +0300

    modify routes

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+#Main Menu
 Route::get('/', "App\Http\Controllers\HomeController@index")->name("home");
 Route::get('/catalog', "App\Http\Controllers\CatalogController@index")->name("catalog");
 Route::get('/about', "App\Http\Controllers\AboutController@index")->name("about");
 Route::get('/contact', "App\Http\Controllers\ContactController@index")->name("contact");
-Route::get('/account', "App\Http\Controllers\AccountController@index")->middleware(["auth", "verified"])->name("account");
 
+#Account
+Route::prefix("account")->middleware(['auth'])->group(function() {
+    Route::get('/', "App\Http\Controllers\AccountController@index")->name("account");
+    Route::post('/update-user', "App\Http\Controllers\AccountController@updateUser")->name("update_user");
+});
+
+#Admin
 Route::prefix("admin")->group(function() {
     Route::get('/', "App\Http\Controllers\AdminController@index")->name("admin");
     Route::get('/login', "App\Http\Controllers\AdminController@showLogin")->name("show_login");
     Route::post('/login', "App\Http\Controllers\AdminController@adminLogin")->name("admin_login");
+    Route::get('/logout', "App\Http\Controllers\AdminController@adminLogout")->name("admin_logout");
+    Route::post('/update-admin', "App\Http\Controllers\AdminController@updateAdmin")->middleware('auth:admin')->name("update_admin");
 });
 
 require __DIR__.'/auth.php';
