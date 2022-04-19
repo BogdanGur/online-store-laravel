8a30c1368fa1d4852c0a3df88fc265ae7bde2a84
BogdanGur <bogur007@gmail.com>
2022-04-19 21:47:15 +0300

    modify account

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -13,8 +13,9 @@
                     <div class="navbar-body">
                         <ul>
                             <li><a href="#"><i class="fas fa-home"></i> Main</a></li>
-                            <li><a href="#">Main</a></li>
-                            <li><a href="#">Main</a></li>
+                            <li><a href="#"><i class="fas fa-shopping-bag"></i> Products</a></li>
+                            <li><a href="#"><i class="fas fa-heart"></i> Liked</a></li>
+                            <li><a href="#"><i class="far fa-credit-card"></i> Orders</a></li>
                         </ul>
                     </div>
                 </aside>
@@ -22,15 +23,81 @@
                     <div class="user-head">
                         <div class="user-info">
                             <div class="user-photo">
-                                <img src="{{ asset("images/photo.jpg") }}" alt="">
+                                @if($user->photo)
+                                    <img src="{{ \Illuminate\Support\Facades\Storage::url("/user_photos/".$user->photo) }}" alt="">
+                                @else
+                                    <img src="{{ asset("images/photo.jpg") }}" alt="">
+                                @endif
                             </div>
                             <div class="user-name">
-                                <div class="uname">Bogdan Gurskyi</div>
-                                <div class="uemail">bogur007@gmail.com</div>
+                                <div class="uname">{{ $user->name }} {{ $user->surname }}</div>
+                                <div class="uemail">{{ $user->email }}</div>
                             </div>
                         </div>
                         <div class="user-right_info">
-                            <div class="uphone">+380501790107</div>
+                            <div class="uphone">{{ $user->phone }}</div>
+                        </div>
+                    </div>
+                    <div class="info-content">
+                        <div class="main-content">
+                            <h3>Main Information</h3>
+
+                            @if(session("success"))
+                                <div class="alert alert-success">{{ session("success") }}</div>
+                            @endif
+                            <div class="sign-up-content">
+                                <form action="{{ route("update_user") }}" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="form-textbox">
+                                        <label for="name">Name</label>
+                                        <input type="text" name="name" id="name" value="{{ $user->name }}">
+                                    </div>
+                                    @if($errors->has("name"))
+                                        <div class="alert alert-danger">{{ $errors->first("name") }}</div>
+                                    @endif
+
+                                    <div class="form-textbox">
+                                        <label for="surname">Surname</label>
+                                        <input type="text" name="surname" id="surname" value="{{ $user->surname }}">
+                                    </div>
+                                    @if($errors->has("surname"))
+                                        <div class="alert alert-danger">{{ $errors->first("surname") }}</div>
+                                    @endif
+
+                                    <div class="form-textbox">
+                                        <label for="email">Email</label>
+                                        <input type="email" name="email" id="email" value="{{ $user->email }}">
+                                    </div>
+                                    @if($errors->has("email"))
+                                        <div class="alert alert-danger">{{ $errors->first("email") }}</div>
+                                    @endif
+
+                                    <div class="form-textbox">
+                                        <label for="phone">Phone</label>
+                                        <input type="text" name="phone" id="phone" value="{{ $user->phone }}">
+                                    </div>
+                                    @if($errors->has("phone"))
+                                        <div class="alert alert-danger">{{ $errors->first("phone") }}</div>
+                                    @endif
+
+                                    <div class="form-textbox">
+                                        <label for="pass">Password</label>
+                                        <input type="password" name="password" id="pass">
+                                    </div>
+
+                                    <div class="form-textbox">
+                                        <label for="photo">Photo</label>
+                                        <input type="file" name="photo" id="photo">
+                                    </div>
+                                    @if($errors->has("photo"))
+                                        <div class="alert alert-danger">{{ $errors->first("photo") }}</div>
+                                    @endif
+
+                                    <div class="form-textbox">
+                                        <input type="submit" name="submit" id="submit" class="submit" value="Update" />
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -45,29 +45,95 @@
                 </div>
                 <div class="navbar-body">
                     <ul>
-                        <li><a href="#"><i class="fas fa-home"></i> Main</a></li>
                         <li><a href="#">Main</a></li>
-                        <li><a href="#">Main</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Site</a></li>
                     </ul>
+                    <br>
+                    <a href="{{ route("admin_logout") }}" class="logout">Logout</a>
                 </div>
             </aside>
             <div class="bgcontent">
                 <div class="user-head">
                     <div class="user-info">
                         <div class="user-photo">
+                        @if($admin->photo)
+                            <img src="{{ \Illuminate\Support\Facades\Storage::url("public/admin_photos/".$admin->photo) }}" alt="">
+                        @else
                             <img src="{{ asset("images/photo.jpg") }}" alt="">
+                        @endif
                         </div>
                         <div class="user-name">
-                            <div class="uname">Bogdan Gurskyi</div>
-                            <div class="uemail">bogur007@gmail.com</div>
+                            <div class="uname">{{ $admin->name }} {{ $admin->surname }}</div>
+                            <div class="uemail">{{ $admin->email }}</div>
                         </div>
                     </div>
                     <div class="user-right_info">
-                        <div class="uphone">+380501790107</div>
+                        <div class="uphone">{{ $admin->phone }}</div>
                     </div>
                 </div>
                 <div class="info-content">
+                    <div class="main-content">
+                        <h3>Admin Information</h3>
 
+                        @if(session("success"))
+                            <div class="alert alert-success">{{ session("success") }}</div>
+                        @endif
+                        <div class="sign-up-content">
+                            <form action="{{ route("update_admin") }}" method="post" enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-textbox">
+                                    <label for="name">Name</label>
+                                    <input type="text" name="name" id="name" value="{{ $admin->name }}">
+                                </div>
+                                @if($errors->has("name"))
+                                    <div class="alert alert-danger">{{ $errors->first("name") }}</div>
+                                @endif
+
+                                <div class="form-textbox">
+                                    <label for="surname">Surname</label>
+                                    <input type="text" name="surname" id="surname" value="{{ $admin->surname }}">
+                                </div>
+                                @if($errors->has("surname"))
+                                    <div class="alert alert-danger">{{ $errors->first("surname") }}</div>
+                                @endif
+
+                                <div class="form-textbox">
+                                    <label for="email">Email</label>
+                                    <input type="email" name="email" id="email" value="{{ $admin->email }}">
+                                </div>
+                                @if($errors->has("email"))
+                                    <div class="alert alert-danger">{{ $errors->first("email") }}</div>
+                                @endif
+
+                                <div class="form-textbox">
+                                    <label for="phone">Phone</label>
+                                    <input type="text" name="phone" id="phone" value="{{ $admin->phone }}">
+                                </div>
+                                @if($errors->has("phone"))
+                                    <div class="alert alert-danger">{{ $errors->first("phone") }}</div>
+                                @endif
+
+                                <div class="form-textbox">
+                                    <label for="pass">Password</label>
+                                    <input type="password" name="password" id="pass">
+                                </div>
+
+                                <div class="form-textbox">
+                                    <label for="photo">Photo</label>
+                                    <input type="file" name="photo" id="photo">
+                                </div>
+                                @if($errors->has("photo"))
+                                    <div class="alert alert-danger">{{ $errors->first("photo") }}</div>
+                                @endif
+
+                                <div class="form-textbox">
+                                    <input type="submit" name="submit" id="submit" class="submit" value="Update" />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
