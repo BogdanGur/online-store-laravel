5861cc0cd30304d3ddc94ff5b1282dba72ed9c14
BogdanGur <bogur007@gmail.com>
2022-04-21 16:11:42 +0300

    Add Product Request Rules And Error Messages

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "name" => "required",
+            "description" => "required|min:5",
+            "price" => "required|integer",
+            "size" => "required",
+            "image.*" => "mimes:jpeg,jpg,png",
+        ];
+    }
+
+    public function messages() {
+        return [
+            "name.required" => "Вы не ввели Название товара",
+            "description.required" => "Вы не ввели описание товара",
+            "description.min" => "Слишком короткое описание",
+            "price.required" => "Вы не ввели цену",
+            "price.integer" => "Не похоже на цену товара",
+            "image.required" => "Выберите фото",
+            "image.mimes" => "Фото должно быть формата: jpeg, jpg, png",
+        ];
+    }
+}
