728b1a702681c5f599f15c07545a36a5fa0911c4
BogdanGur <bogur007@gmail.com>
2022-05-03 19:57:43 +0300

    Add Functions for Like/Dislike produt

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use App\Models\Like;
 use App\Models\Product;
 use App\Models\Site;
 use Illuminate\Http\Request;
@@ -13,5 +14,25 @@ class CatalogController extends Controller
     public function index() {
 
         return view("catalog", ["site" => Site::find(1), "products" => Product::paginate(8), "total_cart" => count(Cart::where("user_id", Auth::id())->get())]);
+    }
+
+    public function likeProduct(Request $request) {
+
+        $is_liked = Like::where("user_id", Auth::id())->where("product_id", $request->product_id)->first();
+        if(!empty($is_liked)) return response()->json(["success" => "Товар уже добавлен в список"]);
+
+        $like = new Like();
+        $like->user_id = Auth::id();
+        $like->product_id = $request->product_id;
+
+        $like->save();
+
+        return response()->json(["success" => "Товар добавлен в избранные"]);
+    }
+
+    public function deleteLike(Request $request) {
+        Like::find($request->id)->delete();
+
+        return response()->json(["success" => "Товар был удален из избранных"]);
     }
 }
