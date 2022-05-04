0cb92f41340eb8468c64958059613121517ea995
BogdanGur <bogur007@gmail.com>
2022-05-04 17:36:21 +0300

    Add Liked Products Array

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserUpdateRequest;
 use App\Models\Cart;
+use App\Models\Like;
 use App\Models\Site;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -16,7 +17,7 @@ class AccountController extends Controller
 
     public function index() {
 
-        return view("account", ["user" => User::find(Auth::id()), "site" => Site::find(1), "total_cart" => count(Cart::where("user_id", Auth::id())->get())]);
+        return view("account", ["user" => User::find(Auth::id()), "site" => Site::find(1), "total_cart" => count(Cart::where("user_id", Auth::id())->get()), "liked" => Like::where("user_id", Auth::id())->get()]);
     }
 
     public function updateUser(UserUpdateRequest $request) {
