85456591cd9d35c43d6dfb03b8b143eec7100fb3
Bogdan Gurskyi <35147686+BogdanGur@users.noreply.github.com>
2022-09-19 11:47:11 +0300

    Add general data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,19 +2,21 @@
 
 namespace App\Providers;
 
+use App\Models\Cart;
+use App\Models\Site;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
-        //
+    public function register() {
+
     }
 
     /**
@@ -22,8 +24,14 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
+
+        $mainData = [
+            "site" => Site::find(1),
+            "total_cart" => count(Cart::where("user_id", Auth::id())->get())
+        ];
+        View::share($mainData);
+
         Paginator::useBootstrap();
     }
 }
